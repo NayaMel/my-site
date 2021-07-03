65dfb1df6bd8e16054276d9f4837cb88317683a1
NayaMel <86608089+NayaMel@users.noreply.github.com>
2021-07-03 21:17:56 +0200

    удалила 2ую цмс

--- admin/template/js/ckeditor/plugins/html5audio/lang/en.js
+++
@@ -1,7 +0,0 @@
-CKEDITOR.plugins.setLang( 'html5audio', 'en', {
-    button: 'Insert HTML5 audio',
-    title: 'HTML5 audio',
-    infoLabel: 'Audio info',
-    urlMissing: 'Audio source URL is missing.',
-    audioProperties: 'Audio properties'
-} );
--- admin/template/js/ckeditor/plugins/html5audio/lang/eu.js
+++
@@ -1,7 +0,0 @@
-CKEDITOR.plugins.setLang( 'html5audio', 'eu', {
-    button: 'Txertatu HTML5 audioa',
-    title: 'HTML5 audioa',
-    infoLabel: 'Audioaren informazioa',
-    urlMissing: 'Audioaren URLak ezin du hutsik egon.',
-    audioProperties: 'Audioaren propietateak'
-} );
